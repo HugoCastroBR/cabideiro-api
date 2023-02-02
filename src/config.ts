6ba311299b582e376155cb202a71ad1ea20542c6
HugoCastroBr <hugoecastro2008@hotmail.com>
2023-02-01 21:09:24 -0300

    feat: swagger and dotenv

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const config = {
+  port: process.env.PORT || 3000,
+};
+
+export default config;
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import env from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  console.log(env.port);
+  const config = new DocumentBuilder()
+    .setTitle('Cabideiro API')
+    .setDescription('The cabideiro API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(env.port);
 }
 bootstrap();
